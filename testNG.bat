904917d113e46ca26b6329ca74ab2d113b544590
Bonguardo <maryse.bonguardo@gmail.com>
2021-05-13 11:46:31 +0200

    essai

--- testNG.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\ExemplesJava\ZEMIS
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*;C:\Users\maryse\.p2\pool\plugins
-java org.testng.TestNG %projectLocation%\testNg.xml
-pause
